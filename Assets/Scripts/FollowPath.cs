c9c4e0a95632641852334431119b909c4098ccb2
KeeganIngersoll <keeeganor@gmail.com>
2019-11-13 14:36:57 -0500

    The game
    The gae

---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] waypoints;
+
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    // Index of current waypoint from which Enemy walks
+    // to the next one
+    private int waypointIndex = 0;
+
+    public float speed;
+
+    public bool ChaseEnabled;
+
+    
+    private void Start()
+    {
+
+        // Set position of Enemy as position of the first waypoint
+
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (ChaseEnabled)
+        {
+            //chasing function
+        }
+        else
+        {
+            // Move Enemy
+            Move();
+        }
+
+    }
+
+    // Method that actually make Enemy walk
+    private void Move()
+    {
+        // If Enemy didn't reach last waypoint it can move
+        // If enemy reached last waypoint then it stops
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+            //finds the angle of rotation towards the next point and turns the sprite towards it.
+            Vector3 vectorToTarget = waypoints[waypointIndex].transform.position - transform.position;
+            float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg)+90;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+
+            // Move Enemy from current waypoint to the next one
+            // using MoveTowards method
+            transform.position = Vector2.MoveTowards(transform.position,
+               waypoints[waypointIndex].transform.position,
+               moveSpeed * Time.deltaTime);
+
+            // If Enemy reaches position of waypoint he walked towards
+            // then waypointIndex is increased by 1
+            // and Enemy starts to walk to the next waypoint
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                print("reached point");
+                waypointIndex += 1;
+                //if the last waypoint is reached then cycle index back to the first 
+                if( waypointIndex== waypoints.Length)
+                {
+                    waypointIndex = 0;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.gameObject.tag == "Player")
+        {
+            print("hehck me up");
+            //trigger chase action
+            startChase();
+        }
+
+    }
+
+    public void startChase()
+    {
+        ChaseEnabled = true;
+        // play anim and sounds so signify the player has been caught
+    }
+
+    public void endChase()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float moveVelocityY, moveVelocityX;
+    public float speed;
+    public Camera cam;
+    private GameObject gob;
+
+
+    //void Start(){
+    //anim = GetComponent<Animator> ();
+    //}
+    void Start()
+    {
+        gob = new GameObject();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        moveVelocityY = 0;
+        moveVelocityX = 0;
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            moveVelocityX = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            moveVelocityX = speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            moveVelocityY = speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            moveVelocityY = -speed;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+      
+
+        }
+
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, moveVelocityY);
+
+
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocityX, GetComponent<Rigidbody2D>().velocity.y);
+
+        gob.transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
+        // point our object at the dummy object
+
+        transform.LookAt(gob.transform, Vector3.forward);
+
+
+
+    }
+}
