577a11a685e49dba59458a2028f4d2bbc8ea75cd
KayNynee <cfitzpa9@student.fitchburgstate.edu>
2019-11-15 02:33:40 -0500

    Beginning of MainMenu Setup
    Code and such for Main Menu

---
+++ Assets/Scripts/UI_Assistant.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Assistant : MonoBehaviour
+{
+    [SerializeField] private TextWriter textWriter;
+    private Text messageText;
+
+    private void Awake()
+    {
+        messageText = transform.Find("Message").Find("messageText").GetComponent<Text>();
+    }
+
+    private void Start()
+    {
+        textWriter.AddWriter(messageText, "Sup World", 1f);
+
+    }
+}
